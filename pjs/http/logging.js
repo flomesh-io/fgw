4741bfe9648d5ca81075b84bdce646d7caf6ce25
Lin Yang <reaver@flomesh.io>
2024-05-31 01:15:09 +0800

    feat: access log
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- pjs/http/logging.js
+++ pjs/http/logging.js
@@ -3,13 +3,16 @@
     loggingEnabled,
     makeLoggingData,
     saveLoggingData,
+    makeAccessLog,
+    printAccessLog,
   } = pipy.solve('lib/logging.js'),
   sampledCounter0 = new stats.Counter('inbound_http_logging_sampled_0'),
   sampledCounter1 = new stats.Counter('inbound_http_logging_sampled_1'),
 ) => (
 
 pipy({
-  _loggingData: null
+  _loggingData: null,
+  _accessLogData: null
 })
 
 .import({
@@ -23,7 +26,8 @@ pipy({
     loggingEnabled && (
       _loggingData = makeLoggingData(msg, __inbound.remoteAddress, __inbound.remotePort, __inbound.destinationAddress, __inbound.destinationPort),
       _loggingData ? sampledCounter1.increase() : sampledCounter0.increase()
-    )
+    ),
+    _accessLogData = makeAccessLog(msg, __inbound.remoteAddress, __inbound.remotePort, __inbound.destinationAddress, __inbound.destinationPort)
   )
 )
 .chain()
@@ -31,6 +35,9 @@ pipy({
   msg => (
     loggingEnabled && _loggingData && (
       saveLoggingData(_loggingData, msg, __service?.name, __target)
+    ),
+    _accessLogData && (
+      printAccessLog(_accessLogData, msg, __service?.name, __target)
     )
   )
 )
--- pjs/lib/logging.js
+++ pjs/lib/logging.js
@@ -12,6 +12,7 @@
 
     address = os.env.REMOTE_LOGGING_ADDRESS,
     tracingLimitedID = os.env.REMOTE_LOGGING_SAMPLED_FRACTION && (os.env.REMOTE_LOGGING_SAMPLED_FRACTION * Math.pow(2, 63)),
+    accessLog = new logging.JSONLogger('console-access-logger').toStdout().log,
     logLogging = address && new logging.JSONLogger('access-logger').toHTTP('http://' + address +
       (os.env.REMOTE_LOGGING_ENDPOINT || '/?query=insert%20into%20log(message)%20format%20JSONAsString'), {
       batch: {
@@ -104,6 +105,70 @@
         logLogging(loggingData),
         isDebugEnabled && console.log('[logging] json:', loggingData)
       ),
+
+      makeAccessLog: (msg, remoteAddr, remotePort, destAddr, destPort) => (
+        () => (
+          {
+            start_time: Date.now(),
+            trace_id: msg.head.headers?.['webgate-tracestate']?.split('-')[2] || '',
+            apikey: msg.head.headers?.['http_apikey'] || '',
+            remote_addr: remoteAddr,
+            remote_port: remotePort,
+            dest_addr: destAddr,
+            dest_port: destPort,
+            username: msg.head.headers?.['username'] || '',
+            tenantcode: msg.head.headers?.['tenantcode'] || '',
+            x_forward_for: msg.head.headers?.['x-forwarded-for'] || '',
+            remote_user: msg.head.headers?.['remote_user'] || '',
+            bytes_sent: msg.body.size + msg.head.size,
+            vhost: msg.head.headers?.['host'] || '',
+            protocol: msg.head?.protocol || '',
+            path: msg.head?.path || '',
+            request_query: msg.head?.query || '',
+            request_length: msg.body.size,
+            method: msg.head?.method || '',
+            http_referrer: msg.head?.headers?.['referer'] || '',
+            user_agent: msg.head?.headers?.['user-agent'] || '',
+            upstream_scheme: msg.head?.scheme || '',
+            http_host: msg.head?.headers?.['host'] || '',
+            request: Object.assign({ reqSize: msg.body.size, body: msg.body.toString('base64') }, msg.head),
+            body_bytes_sent: msg.body.size
+          }
+        )
+      )(),
+
+      printAccessLog: (accessLogData, msg, service, target) => (
+        accessLog({
+          start_time: accessLogData.start_time,
+          trace_id: accessLogData.trace_id,
+          apikey: accessLogData.apikey,
+          remote_addr: accessLogData.remote_addr,
+          username: accessLogData.username,
+          tenantcode: accessLogData.tenantcode,
+          x_forward_for: accessLogData.x_forward_for,
+          remote_user: accessLogData.remote_user,
+          bytes_sent: accessLogData.bytes_sent,
+          response_code: msg.head?.status,
+          vhost: accessLogData.vhost,
+          protocol: accessLogData.protocol,
+          path: accessLogData.path,
+          request_query: accessLogData.request_query,
+          request_length: accessLogData.request_length,
+          duration: Date.now() - accessLogData.start_time,
+          method: accessLogData.method,
+          http_referrer: accessLogData.http_referrer,
+          user_agent: accessLogData.user_agent,
+          upstream_cluster: service || '',
+          upstream_service_time: Date.now(),
+          upstream_status: msg.head?.statusText,
+          upstream_scheme: accessLogData.upstream_scheme,
+          upstream_host: target || '',
+          upstream_uri: 'unknown',
+          http_host: accessLogData.http_host,
+          request: accessLogData.request,
+          body_bytes_sent: accessLogData.body_bytes_sent
+        })
+      )
     }
   )
 

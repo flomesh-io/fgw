dc7e19cd7e753a0e3c14695926001aa016b9ec66
pajama-coder <pajamacoder@flomesh.io>
2025-07-23 11:23:15 +0800

    Separate request timeout and backendRequest timeout

--- test/http-timeout/test.js
+++ test/http-timeout/test.js
@@ -10,6 +10,7 @@ export default function ({ fetch, log }) {
   var host = 'localhost:8000'
   var urlA = 'http://www.test.com/no-timeout'
   var urlB = 'http://www.test.com/timeout'
+  var urlC = 'http://www.test.com/timeout-backend'
 
   return Promise.resolve().then(() => {
     return fetch(host, 'GET', urlA)
@@ -19,9 +20,14 @@ export default function ({ fetch, log }) {
     if (status !== 200) return false
     return fetch(host, 'GET', urlB)
   }).then(res => {
-    if (!res) return false
     var status = res.head.status
     log(urlB, status)
+    if (status !== 504) return false
+    return fetch(host, 'GET', urlC)
+  }).then(res => {
+    if (!res) return false
+    var status = res.head.status
+    log(urlC, status)
     if (status !== 504) return false
     return true
   }).finally(() => {

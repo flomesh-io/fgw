1934ae280facc6ce93c823a0b7dfe8875ba270a5
pajama-coder <pajamacoder@flomesh.io>
2025-08-02 15:46:06 +0800

    Rollback the previous changes for reduced usage of function closures

--- src/modules/backend-selector.js
+++ src/modules/backend-selector.js
@@ -11,12 +11,10 @@ export default function (protocol, listener, rule, makeBalancer) {
   var routeFilters = listenerFilterCaches.get(protocol).get(listener)
   var ruleFilters = makeFilters(routeFilters.outputProtocol || protocol, rule?.filters)
 
-  protocol = ruleFilters.outputProtocol || protocol
-
   var refs = rule?.backendRefs || []
   if (refs.length > 1) {
     var lb = new algo.LoadBalancer(
-      refs.map(ref => makeBackendTarget(ref, protocol, routeFilters, ruleFilters, makeBalancer)),
+      refs.map(ref => makeBackendTarget(ref)),
       {
         key: t => t.id,
         weight: t => t.weight,
@@ -24,32 +22,32 @@ export default function (protocol, listener, rule, makeBalancer) {
     )
     return (hint) => lb.allocate(hint)
   } else {
-    var singleSelection = { target: makeBackendTarget(refs[0], protocol, routeFilters, ruleFilters, makeBalancer) }
+    var singleSelection = { target: makeBackendTarget(refs[0]) }
     return () => singleSelection
   }
-}
 
-function makeBackendTarget(backendRef, protocol, routeFilters, ruleFilters, makeBalancer) {
-  var backendResource = findBackendResource(backendRef)
-  var backendFilters = makeFilters(protocol, backendRef?.filters)
-  var filters = [
-    ...routeFilters,
-    ...ruleFilters,
-    ...backendFilters,
-  ]
-  return {
-    id: backendRef?.name,
-    backendRef,
-    backendResource,
-    weight: backendRef?.weight || 1,
-    pipeline: makeBalancer(backendRef, backendResource, filters, backendFilters.outputProtocol || protocol)
+  function makeBackendTarget(backendRef) {
+    var backendResource = findBackendResource(backendRef)
+    var backendFilters = makeFilters(ruleFilters.outputProtocol || protocol, backendRef?.filters)
+    var filters = [
+      ...routeFilters,
+      ...ruleFilters,
+      ...backendFilters,
+    ]
+    return {
+      id: backendRef?.name,
+      backendRef,
+      backendResource,
+      weight: backendRef?.weight || 1,
+      pipeline: makeBalancer(backendRef, backendResource, filters, backendFilters.outputProtocol || protocol)
+    }
   }
-}
 
-function findBackendResource(backendRef) {
-  if (backendRef) {
-    var kind = backendRef.kind || 'Backend'
-    var name = backendRef.name
-    return resources.find(kind, name)
+  function findBackendResource(backendRef) {
+    if (backendRef) {
+      var kind = backendRef.kind || 'Backend'
+      var name = backendRef.name
+      return resources.find(kind, name)
+    }
   }
 }
